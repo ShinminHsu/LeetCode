77c98f1d9fed6fae541fce64a6a59fa2cd269c9c
ShinminHsu <shinminhsu@gmail.com>
2020-10-16 17:51:45 +0800

    Upload KMP.py

--- Explore/Array_and_String/KMP.py
+++ Explore/Array_and_String/KMP.py
@@ -7,16 +7,43 @@ class Solution:
         :return: int
         """
 
+        if not haystack and not needle:
+            return 0
+        if not needle:
+            return -1
+
+        n = len(haystack)
+        m = len(needle)
+        pi = self.computePrefix(needle)
+        q = 0  # number of characters matched
+
+        for i in range(n):
+            while q > 0 and needle[q] != haystack[i]:
+                q = pi[q-1]  # next character does not match
+            if needle[q] == haystack[i]:
+                q += 1  # next character matches
+            if q == m:
+                return i+1-m
+                # q = pi[q-1]
+
         return -1
 
-    def computePrefix(self, haystack, needle):
+    def computePrefix(self, needle):
         m = len(needle)
-        pi = [0]
+        pi = [0] * m
         k = 0
 
         for q in range(1, m):
-            while k > 0 and needle[k + 1] != pi[q]:
-                k = pi[k]
-            if needle[k + 1] == needle[q]:
+            while k > 0 and needle[k] != needle[q]:
+                k = pi[k-1]
+            if needle[k] == needle[q]:
                 k += 1
             pi[q] = k
+
+        return pi
+
+
+haystack = 'ABABACABABAB'
+needle = 'ABABA'
+s = Solution()
+s.strStr(haystack, needle)
