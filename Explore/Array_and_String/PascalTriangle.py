f5c1f18ced81b150feb47ab7e181a6c72e21b90e
ShinminHsu <shinminhsu@gmail.com>
2020-10-16 09:48:21 +0800

    Upload PascalTriangle.py and spiralOrder.py

---
+++ Explore/Array_and_String/PascalTriangle.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generate(self, numRows):
+        """
+        :parameters: numRows: int
+        :return: List[List[int]]
+        """
+        output = []
+        for i in range(numRows):
+            output.append([1]*(i+1))
+
+        if numRows <= 2:
+            return output
+        
+        for row in range(2, numRows):
+            for col in range(1, row):
+                output[row] = output[row-1][col-1] + output[row-1][col]
+
+        return output
+
+numRows = 1
+s = Solution()
+print(s.generate(numRows))
---
+++ Explore/Array_and_String/spiralOrder.py
@@ -0,0 +1,46 @@
+class Solution:
+    def spiralOrder(self, matrix):
+        """
+        :parameters: matrix: List[List[int]]
+        :return: List[int]
+        """
+        if not matrix: return []
+
+        nrow, ncol = len(matrix), len(matrix[0])
+        size = nrow * ncol
+        r, l, u, d = ncol - 1, 0, 0, nrow - 1  # boundaries
+        ouput = []
+
+        while len(ouput) < size:
+
+            # top row, turn right
+            if len(ouput) < size:
+                ouput.extend(matrix[u][j] for j in range(l, r+1))
+            u += 1
+            
+            # right column, turn down
+            if len(ouput) < size:
+                ouput.extend(matrix[i][r] for i in range(u, d+1))
+            r -= 1
+            
+            # bottom row, turn left
+            if len(ouput) < size:
+                ouput.extend(matrix[d][j] for j in range(r, l-1, -1))
+            d -= 1
+            
+            # left column, turn up
+            if len(ouput) < size:
+                ouput.extend(matrix[i][l] for i in range(d, u-1, -1))
+            l += 1
+
+        return ouput
+
+matrix = [
+ [ 1, 2, 3, 10, 13 ],
+ [ 4, 5, 6, 11, 14 ],
+ [ 7, 8, 9, 12, 15 ],
+ [ 16, 17, 18, 19, 20]
+]
+
+s = Solution()
+print(s.spiralOrder(matrix))
