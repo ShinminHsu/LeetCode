6594573977ad246b41f75e00bd33ae8f84acfa61
Shinmin Hsu <shinminhsu@gmail.com>
2020-10-19 22:08:53 +0800

    Upload minSubArrayLen.py

--- Explore/Array_and_String/minSubArrayLen.py
+++ Explore/Array_and_String/minSubArrayLen.py
@@ -5,17 +5,25 @@ class Solution():
         :param target: List[int]
         :return: int
         """
+        if not nums:
+            return 0
 
         i, j, n = 0, 0, len(nums)
         minLen = float("inf")
         curSum = 0
 
         while j < n:
-            if j < n and curSum < s:
+            while j < n and curSum < s:
                 curSum += nums[j]
                 j += 1
-            elif i < j and curSum > s:
+            while i < j and curSum >= s:
                 curSum -= nums[i]
-                i -= 1
+                i += 1
+            minLen = min(minLen, j-i+1)
 
-        
+        return minLen if minLen <= n else 0
+
+solution = Solution()
+nums = [2,3,1,2,4]
+s = 7
+print(solution.minSubArrayLen(s, nums))
