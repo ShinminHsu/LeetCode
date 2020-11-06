4a279a9dc0937cc0369be500ff9984b674df40ed
ShinminHsu <shinminhsu@gmail.com>
2020-11-06 18:02:03 +0800

    Upload unfinished decodeString.py

--- Explore/Queue_and_Stack/decodeString.py
+++ Explore/Queue_and_Stack/decodeString.py
@@ -1,6 +1,7 @@
+import unittest
+
 class Solution:
     def decodeString(self, s: str) -> str:
-        output = ""
         stack, substring, curNum = [], "", 0
 
         for c in s:
@@ -14,10 +15,13 @@ class Solution:
             elif c == "]":
                 top = stack.pop()
                 if isinstance(top, int):
-                    output += substring * top  # pop curNum stored in the "[" step
-                    substring = ""
+                    substring = substring * top  # pop curNum stored in the "[" step
                 else:
-                    output = (top + output) * stack.pop()
+                    substring = top + substring
+
+                stack.append(substring)  # put the processed substring onto the top
+                substring = ""
+                print(stack)
 
             elif c == "[":
                 # put the previous number onto the stack and set curNum as zero
@@ -27,11 +31,37 @@ class Solution:
             else:
                 substring += c
 
-        if stack:
-            output = stack.pop() + output
+        if substring:
+            stack.append(substring)
 
-        return output + substring
+        print(stack)
+        output = ""
+        while stack:
+            top = stack.pop()
 
-string = "3[2[cd]xyz]"
+            if isinstance(top, int):
+                curNum = top
+                output = curNum * output
+            else:
+                output = top + output
+
+        return output
+
+class SolutionTestCase(unittest.TestCase):
+    def testCase(self):
+
+        stringList = ["3[a]2[bc]", "3[a2[c]]",  "2[abc]3[cd]ef" , "abc3[cd]xyz", "3[z]2[2[y]pq4[2[jk]e1[f]]]ef"]
+        answerList = ["aaabcbc",   "accaccacc", "abcabccdcdcdef", "abccdcdcdxyz", "zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef"]
+
+        sol = Solution()
+
+        for s, a in zip(stringList, answerList):
+            result = sol.decodeString(s)
+            self.assertEqual(result, a) 
+
+
+
+
+string = "3[z]2[2[y]pq4[2[jk]e1[f]]]ef"
 solution = Solution()
 print(solution.decodeString(string))
