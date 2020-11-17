1d8ff47e4239c5816d181ff0dbaf0710bc0b6719
Shinmin Hsu <shinminhsu@gmail.com>
2020-11-17 23:37:54 +0800

    Finished PalindromeLinkedList.py and Uploaded RemoveLinkedListElements.py

--- Explore/Linked_List/PalindromeLinkedList.py
+++ Explore/Linked_List/PalindromeLinkedList.py
@@ -7,11 +7,20 @@ class ListNode:
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
 
-        reversed_head = head
+        fast = slow = head
+        rev_head = None
 
-        while head:
+        # find the middle of the Linked List (slow will be the middle)
+        while fast and fast.next:
+            fast = fast.next.next
+            rev_head, rev_head.next, slow = slow, rev_head, slow.next
 
+        # if fast exists, the rest half start from slow.next
+        if fast:
+            slow = slow.next
 
-            reversed_head.next = head.next
+        # compare the rest half (start from slow) and the reversed half
+        while rev_head and slow.val == rev_head.val:
+            slow, rev_head = slow.next, rev_head.next
 
-            head = head.next
+        return not rev_head
--- Explore/Linked_List/RemoveLinkedListElements.py
+++ Explore/Linked_List/RemoveLinkedListElements.py
@@ -4,17 +4,24 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
+
+    def removeElements_recursive(self, head: ListNode, val: int) -> ListNode:
+        if not head:
+            return None
+        head.next = self.removeElements_recursive(head.next, val)
+        return head.next if head.val == val else head
+
     def removeElements(self, head: ListNode, val: int) -> ListNode:
         if not head:
             return None
 
-        node = head
+        dummy_head = ListNode(-1, head)
+        node = dummy_head
 
         while node.next:
-
             if node.next.val == val:
                 node.next = node.next.next
-                
-            node = node.next
+            else:
+                node = node.next
 
-        return head
+        return dummy_head.next
