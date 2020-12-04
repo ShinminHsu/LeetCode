ba2d4592a09a9b47efa1958593d562dd2bf91c8b
ShinminHsu <shinminhsu@gmail.com>
2020-12-04 18:00:42 +0800

    Upload Hash_Table

---
+++ Explore/Hash_Table/hashSet.py
@@ -0,0 +1,32 @@
+class MyHashSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.hashSet = set()
+        
+
+    def add(self, key: int) -> None:
+        if not self.contains(key):
+            self.hashSet.add(key)
+        
+
+    def remove(self, key: int) -> None:
+        if self.contains(key):
+            self.hashSet.remove(key)
+        
+
+    def contains(self, key: int) -> bool:
+        """
+        Returns true if this set contains the specified element
+        """
+        return (key in self.hashSet)
+
+
+# Your MyHashSet object will be instantiated and called as such:
+obj = MyHashSet()
+obj.add(5)
+print(obj.contains(5))
+obj.remove(5)
+print(obj.contains(5))
